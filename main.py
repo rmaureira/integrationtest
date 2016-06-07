f965bc3f58f2d213b7a2afca8c316e5b8903c485
Robinson Maureira Castillo <rmaureira@gmail.com>
2016-06-07 19:46:36 -0400

    Change host binding

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = webapp2.WSGIApplication([
 
 def main():
     from paste import httpserver
-    httpserver.serve(app, host='127.0.0.1', port='8080')
+    httpserver.serve(app, host='0.0.0.0', port='8080')
 
 if __name__ == '__main__':
     main()
